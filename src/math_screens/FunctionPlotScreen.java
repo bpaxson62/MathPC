7335da9ee8e12d023c5d146b702872ae18a07b21
gbkuhn <kuhn.geoff@gmail.com>
2015-04-12 18:13:24 -0400

    more function options

--- src/math_screens/FunctionPlotScreen.java
+++ src/math_screens/FunctionPlotScreen.java
@@ -11,7 +11,6 @@ import javafx.scene.layout.GridPane;
 
 import javafx.scene.text.*;
 
-
 /**
  * Created by geoff on 4/10/2015.
  */
@@ -21,32 +20,60 @@ public class FunctionPlotScreen extends MathGame{
     static double eq2=0;
 
     public FunctionPlotScreen(){
-
+///
         Text t = new Text();
-        t.setFont(new Font(50));
+        t.setFont(new Font(40));
         t.setWrappingWidth(200);
         t.setTextAlignment(TextAlignment.JUSTIFY);
-        t.setText("y=mx+b");
-
+        t.setText("f(x)=mx+b");
+        
+        Text t2 = new Text();
+        t2.setFont(new Font(50));
+        t2.setWrappingWidth(200);
+        t2.setTextAlignment(TextAlignment.JUSTIFY);
+        t2.setText("f(x)=x^2");
+///
         final TextField function_field = new TextField("0");
         final TextField function_field2 = new TextField("0");
         Button submit = new Button("Submit");
 
+        final TextField function_field3 = new TextField("0");
+        Button submit2 = new Button("Submit");
+///
         GridPane text_grid = new GridPane();
         GridPane grid = new GridPane();
-
+        
+        GridPane text_grid2 = new GridPane();
+        GridPane grid2 = new GridPane();
+///
         grid.setVgap(30);
         grid.setHgap(50);
 
         grid.setLayoutX(750);
-        grid.setLayoutY(250);
+        grid.setLayoutY(120);
 
         text_grid.setVgap(30);
         text_grid.setHgap(50);
 
         text_grid.setLayoutX(750);
-        text_grid.setLayoutY(180);
+        text_grid.setLayoutY(70);
+///        
+        grid2.setVgap(30);
+        grid2.setHgap(50);
 
+        grid2.setLayoutX(750);
+        grid2.setLayoutY(320);
+
+        text_grid2.setVgap(30);
+        text_grid2.setHgap(50);
+
+        text_grid2.setLayoutX(750);
+        text_grid2.setLayoutY(270);
+///       
+        
+        text_grid2.setConstraints(t2, 0, 0);
+        text_grid2.setConstraints(function_field3, 0, 1);
+///
         GridPane.setConstraints(t,0,0);
 
         GridPane.setConstraints(function_field,0,0);
@@ -67,7 +94,6 @@ public class FunctionPlotScreen extends MathGame{
         //creating the chart
         final LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
 
-
         /////
         //event handler
         submit.setOnAction(new EventHandler<ActionEvent>() {
@@ -81,12 +107,9 @@ public class FunctionPlotScreen extends MathGame{
                 //System.out.print(get_root1()+" "+get_root2());
                 //root_result_1.setText(String.valueOf(get_root1()));
                 //root_result_2.setText(String.valueOf(get_root2()));
-
-              
-
+               
                 lineChart.setPrefHeight(700);
                 lineChart.setPrefWidth(700);
-
                 
                 lineChart.setTitle("Plotting Y equation");
                 //defining a series
@@ -94,21 +117,7 @@ public class FunctionPlotScreen extends MathGame{
                 series.setName("Plotted points");
                 //populating the series with data
                 series.getData().add(new XYChart.Data(0, eq1));
-                series.getData().add(new XYChart.Data(eq2, eq1+1));
-
-                /*
-                series.getData().add(new XYChart.Data(2, 14));
-                series.getData().add(new XYChart.Data(3, 15));
-                series.getData().add(new XYChart.Data(4, 24));
-                series.getData().add(new XYChart.Data(5, 34));
-                series.getData().add(new XYChart.Data(6, 36));
-                series.getData().add(new XYChart.Data(7, 22));
-                series.getData().add(new XYChart.Data(8, 45));
-                series.getData().add(new XYChart.Data(9, 43));
-                series.getData().add(new XYChart.Data(10, 17));
-                series.getData().add(new XYChart.Data(11, 29));
-                series.getData().add(new XYChart.Data(12, 25));
-                */
+                series.getData().add(new XYChart.Data(eq2, eq1+1));               
 
                 lineChart.getData().add(series);
                 getChildren().addAll(lineChart);
