636fb48c16df139096b5d2b17997f6ef637878c0
gbkuhn <kuhn.geoff@gmail.com>
2015-04-18 16:05:38 -0400

    Much improved function plotter

--- src/launch/Configurations.java
+++ src/launch/Configurations.java
@@ -7,7 +7,7 @@ public final class Configurations  {
 
     //Window Sizes
     public static final int MAIN_SCREEN_WIDTH=1400;
-    public static final int MAIN_SCREEN_HEIGHT=700;
+    public static final int MAIN_SCREEN_HEIGHT=720;
     public static final int SCORE_SCREEN_WIDTH=0;
     public static final int SCORE_SCREEN_HEIGHT=0;
     public static final int WINDOW_BOARDER=3;
--- src/math_screens/FunctionPlotScreen.java
+++ src/math_screens/FunctionPlotScreen.java
@@ -28,25 +28,37 @@ public class FunctionPlotScreen extends MathGame{
         t.setText("f(x)=mx+b");
         
         Text t2 = new Text();
-        t2.setFont(new Font(50));
+        t2.setFont(new Font(40));
         t2.setWrappingWidth(200);
         t2.setTextAlignment(TextAlignment.JUSTIFY);
-        t2.setText("f(x)=x^2");
+        t2.setText("f(x)=n^x");
+        
+        Text t3 = new Text();
+        t3.setFont(new Font(40));
+        t3.setWrappingWidth(200);
+        t3.setTextAlignment(TextAlignment.JUSTIFY);
+        t3.setText("f(x)=sqrt(x)");
 ///
         final TextField function_field = new TextField("0");
         final TextField function_field2 = new TextField("0");
         Button submit = new Button("Submit");
 
-        final TextField function_field3 = new TextField("0");
+        final TextField function_field3 = new TextField("x");
         Button submit2 = new Button("Submit");
+        
+        final TextField function_field4 = new TextField("0");
+        Button submit3 = new Button("Submit");
 ///
         GridPane text_grid = new GridPane();
         GridPane grid = new GridPane();
         
         GridPane text_grid2 = new GridPane();
         GridPane grid2 = new GridPane();
+        
+        GridPane text_grid3 = new GridPane();
+        GridPane grid3 = new GridPane();
 ///
-        grid.setVgap(30);
+        grid.setVgap(10);
         grid.setHgap(50);
 
         grid.setLayoutX(750);
@@ -58,22 +70,39 @@ public class FunctionPlotScreen extends MathGame{
         text_grid.setLayoutX(750);
         text_grid.setLayoutY(70);
 ///        
-        grid2.setVgap(30);
+        grid2.setVgap(10);
         grid2.setHgap(50);
 
         grid2.setLayoutX(750);
-        grid2.setLayoutY(320);
+        grid2.setLayoutY(250);
 
         text_grid2.setVgap(30);
         text_grid2.setHgap(50);
 
         text_grid2.setLayoutX(750);
-        text_grid2.setLayoutY(270);
-///       
+        text_grid2.setLayoutY(210);
+///     
+        grid3.setVgap(10);
+        grid3.setHgap(50);
+
+        grid3.setLayoutX(750);
+        grid3.setLayoutY(350);
+
+        text_grid3.setVgap(30);
+        text_grid3.setHgap(50);
+
+        text_grid3.setLayoutX(750);
+        text_grid3.setLayoutY(300);        
+///
         
         text_grid2.setConstraints(t2, 0, 0);
-        text_grid2.setConstraints(function_field3, 0, 1);
-        text_grid2.setConstraints(submit2, 1, 0);
+        text_grid2.setConstraints(function_field3, 1, 1);
+        text_grid2.setConstraints(submit2, 0, 1);
+        
+        text_grid3.setConstraints(t3, 0, 0);
+        text_grid3.setConstraints(function_field4, 1, 1);
+        text_grid3.setConstraints(submit3, 0, 1);
+        
 ///
         GridPane.setConstraints(t,0,0);
 
@@ -82,20 +111,24 @@ public class FunctionPlotScreen extends MathGame{
         GridPane.setConstraints(submit,0,1);
         
 ///
-
         getChildren().add(grid);
         getChildren().add(text_grid);
 ///
         getChildren().add(grid2);
         getChildren().add(text_grid2);
 ///
-        grid2.getChildren().addAll(function_field,submit2);
-        text_grid2.getChildren().addAll(t2);
-        
+        getChildren().add(grid3);
+        getChildren().add(text_grid3);        
+///
+        grid2.getChildren().addAll(function_field3,submit2);
+        text_grid2.getChildren().addAll(t2);  
 ///
         grid.getChildren().addAll(function_field,function_field2, submit);
         text_grid.getChildren().addAll(t);
-
+///
+        grid3.getChildren().addAll(function_field4,submit3);
+        text_grid3.getChildren().addAll(t3);   
+///
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("X-axis");
@@ -123,10 +156,67 @@ public class FunctionPlotScreen extends MathGame{
                 lineChart.setTitle("Plotting Y equation");
                 //defining a series
                 XYChart.Series series = new XYChart.Series();
-                series.setName("Plotted points");
+                series.setName("f(x)=mx+b");
                 //populating the series with data
                 series.getData().add(new XYChart.Data(0, eq1));
                 series.getData().add(new XYChart.Data(eq2, eq1+1));               
+
+                lineChart.getData().add(series);
+                getChildren().addAll(lineChart);
+
+            }
+        });
+        
+        submit2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                // label.setText("Accepted");
+
+                double eq3 = Double.parseDouble(function_field3.getText());
+                //double eq2 = Double.parseDouble(function_field2.getText());
+                //System.out.println("Equation Entered:  "+ eq);
+                //quadraticEquationRoots(op1, op2, op3);
+                //System.out.print(get_root1()+" "+get_root2());
+                //root_result_1.setText(String.valueOf(get_root1()));
+                //root_result_2.setText(String.valueOf(get_root2()));         
+                lineChart.setPrefHeight(700);
+                lineChart.setPrefWidth(700);
+                //lineChart.setTitle("Plotting Y equation");
+                //defining a series
+                XYChart.Series series = new XYChart.Series();
+                series.setName("f(x)=n^x");
+                //populating the series with data
+                for (int i=0;i<5; i++){
+                	series.getData().add(new XYChart.Data(i, i*eq3*i));
+                }
+               // series.getData().add(new XYChart.Data(eq2, eq1+1));               
+
+                lineChart.getData().add(series);
+                getChildren().addAll(lineChart);
+
+            }
+        });
+        submit3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                // label.setText("Accepted");
+
+                double eq4 = Double.parseDouble(function_field4.getText());
+                //double eq2 = Double.parseDouble(function_field2.getText());
+                //System.out.println("Equation Entered:  "+ eq);
+                //quadraticEquationRoots(op1, op2, op3);
+                //System.out.print(get_root1()+" "+get_root2());
+                //root_result_1.setText(String.valueOf(get_root1()));
+                //root_result_2.setText(String.valueOf(get_root2()));         
+                lineChart.setPrefHeight(700);
+                lineChart.setPrefWidth(700);
+                //lineChart.setTitle("Plotting Y equation");
+                //defining a series
+                XYChart.Series series = new XYChart.Series();
+                series.setName("f(x)=sqrt(x)");
+                //populating the series with data
+                for  (int i=(int) eq4;i<10; i++){
+                	series.getData().add(new XYChart.Data(i,Math.sqrt(i)));
+                }
+               // series.getData().add(new XYChart.Data(eq2, eq1+1));               
 
                 lineChart.getData().add(series);
                 getChildren().addAll(lineChart);
